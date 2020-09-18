b69834615d40390ef32982644df7a25341879187
Fabricio <fabricio@uniplaclages.edu.br>
2020-09-18 17:13:56 -0300

    implementações referentes ao curso de modelando domínios ricos

---
+++ PaymentContext.Domain/Entities/Subscription.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PaymentContext.Domain.Entities
+{
+    public class Subscription
+    {
+        private IList<Payment> _payments;
+        public Subscription(DateTime? expireDate)
+        {
+            CreateDate = DateTime.Now;
+            LastUpdateDate = DateTime.Now;
+            ExpireDate = expireDate;
+            _payments = new List<Payment>();
+            Active = true;
+        }
+
+        public DateTime CreateDate { get; private set; }
+        public DateTime LastUpdateDate { get; private set; }
+        public DateTime? ExpireDate { get; private set; }
+        public IReadOnlyCollection<Payment> Payments { get { return _payments.ToArray(); } }
+        public bool Active { get; private set; }
+
+        public void AddPayment(Payment payment)
+        {
+            _payments.Add(payment);
+        }
+
+        public void Activate(){
+            Active = true;
+            LastUpdateDate = DateTime.Now;
+        }
+
+        public void Inactivate(){
+            Active = false;
+            LastUpdateDate = DateTime.Now;
+        }
+    }
+}
---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void Init()
+        {
+            // var student = new Student("Fabricio", "Borges", "123456", );
+        }
+    }
+}
